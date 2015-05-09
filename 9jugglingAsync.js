721908a69e1ebc5fe3b8ff07c2e9dd18964c1872
kevdez <kernan373@gmail.com>
2015-05-08 17:01:05 -0700

    9. Juggling Async completed v2

--- 9jugglingAsync.js
+++ 9jugglingAsync.js
@@ -1,46 +1,30 @@
 var http = require('http');
 var bl = require('bl');
 
-var url1 = process.argv[2];
-var url2 = process.argv[3];
-var url3 = process.argv[4];
-
 var count = 3;
 
-var data1;
-var data2;
-var data3;
+var results = [];
 
-http.get(url1, function (response) {
-	response.pipe(bl(function (err, data) {
-		data1 = data.toString();
-		count = count - 1;
-		if (count == 0) {
-	console.log(data1);
-	console.log(data2);
-	console.log(data3);
+function printResults () {
+	for (var i = 0; i < 3; i++)
+		console.log(results[i]);
 }
-	}))
-});
-http.get(url2, function (response) {
-	response.pipe(bl(function (err, data) {
-		data2 = data.toString();
-		count = count - 1;
-		if (count == 0) {
-	console.log(data1);
-	console.log(data2);
-	console.log(data3);
+
+function httpGet (index) {
+	http.get(process.argv[2 + index], function (response){
+		response.pipe(bl(function (err, data) {
+			if (err)
+				return console.error(err);
+
+			results[index] = data.toString();
+			count = count - 1;
+
+			if (count == 0)
+				printResults();
+		}))
+	})
 }
-	}))
-});
-http.get(url3, function (response) {
-	response.pipe(bl(function (err, data) {
-		data3 = data.toString();
-		count = count - 1;
-		if (count == 0) {
-	console.log(data1);
-	console.log(data2);
-	console.log(data3);
+
+for(var i = 0; i < 3; i++) {
+	httpGet(i);
 }
-	}))
-});
