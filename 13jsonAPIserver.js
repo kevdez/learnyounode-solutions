8c3ceb027277e0947f8dfcaf79191acd0776164b
kevdez <kernan373@gmail.com>
2015-05-12 12:19:52 -0700

    13. JSON API Server complete v2

--- 13jsonAPIserver.js
+++ 13jsonAPIserver.js
@@ -1,6 +1,11 @@
 var http = require('http');
 var url = require('url');
 
+// requests like:
+// "/api/parsetime?iso=2013-08-10T12:10:15.474Z"
+// or
+// "/api/unixtime?iso=2013-08-10T12:10:15.474Z"
+
 var server = http.createServer(function (req, res) {
 	if (req.method != 'GET')
 		return res.end('send me a GET\n');
@@ -10,17 +15,16 @@ var server = http.createServer(function (req, res) {
 	var requestObj = url.parse(req.url, true);
 	if (requestObj.pathname == '/api/parsetime') {
 		var date = new Date (requestObj.query.iso);
-		var jsonObject = {
+		res.end(JSON.stringify({
 			'hour'		:	date.getHours(),
 			'minute'	:	date.getMinutes(),
 			'second'	:	date.getSeconds()
-			};
-		res.end(JSON.stringify(jsonObject));
+			}));
 	} else if (requestObj.pathname == '/api/unixtime') {
 		var date = new Date (requestObj.query.iso);
-
 		res.end(JSON.stringify({ "unixtime" : date.getTime()}));
 	}
 });
-
 server.listen(process.argv[2]);
+
+console.log("Server running at http://127.0.0.1:" + process.argv[2] + "/");
